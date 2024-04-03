c9a0021780e817df3596b7afa94d25b4be9fdfe4
codemaestro-dev <codemaestro101@gmail.com>
2024-04-03 14:08:48 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,44 +1,20 @@
-const containerBodyQuestion = document.querySelector(".container__body__question");
-const containerBodyQuestionHeading = document.querySelector(
-    ".container__body__question h2"
-)
+const containerBodyQuestion = document.querySelector(
+  ".container__body__question"
+);
 const answerA = document.getElementById("answers-A");
 const answerB = document.getElementById("answers-B");
 const answerC = document.getElementById("answers-C");
 const answerD = document.getElementById("answers-D");
 
+const containerBodyAnswersIndicator = document.querySelectorAll(
+  ".container__body__answers--indicator"
+);
 const containerBodyAnswersIndicatorLabel = document.querySelectorAll(
   ".container__body__answers--indicator label"
 );
 const questionNumber = document.querySelector("#question-number");
-questionNumber.innerHTML = 1;
 const previousButton = document.querySelector("#previous-button");
 const nextButton = document.querySelector("#next-button");
-
-console.log(containerBodyQuestion);
-
-if (answerA) {
-  answerA.addEventListener("click", () => {
-    console.log(answerA.checked)
-  });
-}
-if (answerB) {
-  answerB.addEventListener("click", () => {
-    console.log(answerB.checked)
-  });
-}
-if (answerC) {
-  answerC.addEventListener("click", () => {
-    console.log(answerC.checked)
-  });
-}
-if (answerD) {
-  answerD.addEventListener("click", () => {
-    console.log(answerD.checked)
-  });
-}
-
-
 
 const questions = [
   {
@@ -63,26 +39,16 @@ const questions = [
   },
   {
     question: "3. What does JS stand for?",
-    options: [
-      "Junior Stylesheet",
-      "Java Styles",
-      "Java Script",
-      "JavaScript",
-    ],
+    options: ["Junior Stylesheet", "Java Styles", "Java Script", "JavaScript"],
     answer: "JavaScript",
   },
   {
-    question: "4. Which is a JavaScript Library",
-    options: [
-      "React",
-      "Bootstrap",
-      "Bulma",
-      "Laravel",
-    ],
+    question: "4. Which is a JavaScript Library?",
+    options: ["React", "Bootstrap", "Bulma", "Laravel"],
     answer: "React",
   },
   {
-    question: "5. Who invented HTML and what year",
+    question: "5. Who invented HTML and what year?",
     options: [
       " HÃ¥kon Wium Lie, 1994",
       "Brendan Eich, 1995",
@@ -93,15 +59,222 @@ const questions = [
   },
 ];
 
-console.log(questions[0].options[0]);
+// Logics
 
+// if (answerA) {
+//   answerA.addEventListener("click", () => {
+//     console.log(answerA.checked);
+//   });
+// }
+// if (answerB) {
+//   answerB.addEventListener("click", () => {
+//     console.log(answerB.checked);
+//   });
+// }
+// if (answerC) {
+//   answerC.addEventListener("click", () => {
+//     console.log(answerC.checked);
+//   });
+// }
+// if (answerD) {
+//   answerD.addEventListener("click", () => {
+//     console.log(answerD.checked);
+//   });
+// }
 
+function getUserAnswer() {
+  for (let i = 0; i < containerBodyAnswersIndicator.length; i++) {
+    if (answerA) {
+      answerA.addEventListener("click", () => {
+        console.log(answerA.checked);
+        containerBodyAnswersIndicator[0].style = "background-color: #74f412";
+        if (questions[0].options[0] === questions[0].answer) {
+          console.log("correct answer");
+        }
+      });
+      answerA.addEventListener("blur", () => {
+        containerBodyAnswersIndicator[0].style = "backgroound-color: #00ffb0";
+      });
+    }
+    if (answerB) {
+      answerB.addEventListener("click", () => {
+        console.log(answerB.checked);
+        containerBodyAnswersIndicator[1].style = "background-color: #74f412";
+        console.log(questions[0].options[1]);
+      });
+      answerB.addEventListener("blur", () => {
+        containerBodyAnswersIndicator[1].style = "backgroound-color: #00ffb0";
+      });
+    }
+    if (answerC) {
+      answerC.addEventListener("click", () => {
+        console.log(answerC.checked);
+        containerBodyAnswersIndicator[2].style = "background-color: #74f412";
+        console.log(questions[0].options[2]);
+      });
+      answerC.addEventListener("blur", () => {
+        containerBodyAnswersIndicator[2].style = "backgroound-color: #00ffb0";
+      });
+    }
+    if (answerD) {
+      answerD.addEventListener("click", () => {
+        console.log(answerD.checked);
+        containerBodyAnswersIndicator[3].style = "background-color: #74f412";
+        console.log(questions[0].options[3]);
+      });
+      answerD.addEventListener("blur", () => {
+        containerBodyAnswersIndicator[3].style = "backgroound-color: #00ffb0";
+      });
+    }
+    break;
+  }
+}
 
+getUserAnswer();
+
+// function getCheckedAnswer() {
+//     let userAnswer = "";
+//     if(answerA.checked) {
+//         userAnswer = answerA.value;
+//     } else if(answerB.checked) {
+//         userAnswer = answerB.value;
+//     } else if(answerC.checked) {
+//         userAnswer = answerC.value;
+//     } else if(answerD.checked) {
+//         userAnswer = answerD.value;
+//     }
+//     console.log(userAnswer);
+//     return(userAnswer);
+// }
+
+// getCheckedAnswer();
+
+let number = 0;
+containerBodyQuestion.innerHTML = `<h1>${questions[number].question}</h1>`;
 
 previousButton.addEventListener("click", () => {
-  console.log('previousButton');  
-})
+  console.log(number);
+  if (number > 0) {
+    number--;
+    containerBodyQuestion.innerHTML = `<h1>${questions[number].question}</h1>`;
+  } else {
+    number = 5;
+  }
+
+  for (let i = 0; i < containerBodyAnswersIndicatorLabel.length; i++) {
+    if (containerBodyQuestion.innerText === "1. What does HTML stand for?") {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[0].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "2. What does CSS stand for?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[1].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "3. What does JS stand for?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[2].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "4. Which is a JavaScript Library?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[3].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "5. Who invented HTML and what year?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[4].options[i];
+    } else {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[0].options[i];
+    }
+  }
+
+  console.log(containerBodyQuestion.innerText);
+});
 
 nextButton.addEventListener("click", () => {
-  console.log('nextButton');  
-})
+  console.log(number);
+  if (number < 5) {
+    number++;
+    containerBodyQuestion.innerHTML = `<h1>${questions[number].question}</h1>`;
+  } else {
+    number = 0;
+  }
+
+  for (let i = 0; i < containerBodyAnswersIndicatorLabel.length; i++) {
+    if (containerBodyQuestion.innerText === "1. What does HTML stand for?") {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[0].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "2. What does CSS stand for?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[1].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "3. What does JS stand for?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[2].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "4. Which is a JavaScript Library?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[3].options[i];
+    } else if (
+      containerBodyQuestion.innerText === "5. Who invented HTML and what year?"
+    ) {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[4].options[i];
+    } else {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[0].options[i];
+    }
+  }
+
+  console.log(containerBodyQuestion.innerText);
+});
+
+
+for (let i = 0; i < containerBodyAnswersIndicatorLabel.length; i++) {
+    if (containerBodyQuestion.innerText === '1. What does HTML stand for?') {
+        containerBodyAnswersIndicatorLabel[i].innerHTML = questions[0].options[i];
+  } else if (containerBodyQuestion.innerText === '2. What does CSS stand for?') {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[1].options[i];
+    } else if (containerBodyQuestion.innerText === '3. What does JS stand for?') {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[2].options[i];
+    } else if (containerBodyQuestion.innerText === '4. Which is a JavaScript Library?') {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[3].options[i];
+    } else if (containerBodyQuestion.innerText === '5. Who invented HTML and what year?') {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[4].options[i];
+    } else {
+      containerBodyAnswersIndicatorLabel[i].innerHTML = questions[0].options[i];
+    }
+  }
+  
+  console.log(containerBodyQuestion.innerText)
+
+switch (containerBodyQuestion.innerText) {
+  case questions[0].question:
+    number = 0;
+    break;
+  case questions[1].question:
+    number = 1;
+    break;
+  case questions[2].question:
+    number = 2;
+    break;
+  case questions[3].question:
+    number = 3;
+    break;
+  case questions[4].question:
+    number = 4;
+    break;
+}
+
+switch (containerBodyQuestion.innerText) {
+  case questions[0].question:
+    questionNumber.innerText = 1;
+    break;
+  case questions[1].question:
+    questionNumber.innerText = 2;
+    break;
+  case questions[2].question:
+    questionNumber.innerText = 3;
+    break;
+  case questions[3].question:
+    questionNumber.innerText = 4;
+    break;
+  case questions[4].question:
+    questionNumber.innerText = 5;
+    break;
+}
